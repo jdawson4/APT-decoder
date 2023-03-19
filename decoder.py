b5106bd9a2d7584d31283ab37f85b2887643cafc
Jacob Dawson <jacob.dawson@me.com>
2023-03-19 00:00:37 -0400

    remove original code

--- decoder.py
+++ decoder.py
@@ -116,48 +116,6 @@ def modified(filename, outputFolder):
     print(f"Writing image to {output_path}")
     image.save(output_path)
 
-
-def original(filename):
-    fs, data = wav.read(filename)
-    if len(data.shape) == 2:
-        print(f"Data in two channels, using the first one")
-        data = data[:, 0]  # only need 1 channel
-
-    resample = 4
-    data = data[::resample]
-    fs = fs // resample
-
-    def hilbert(data):
-        analytical_signal = signal.hilbert(data)
-        amplitude_envelope = np.abs(analytical_signal)
-        return amplitude_envelope
-
-    data_am = hilbert(data)
-    frame_width = int(0.5 * fs)
-    w, h = frame_width, data_am.shape[0] // frame_width
-    image = Image.new("RGB", (w, h))
-    px, py = 0, 0
-    for p in range(data_am.shape[0]):
-        lum = int(data_am[p] // 32 - 32)
-        if lum < 0:
-            lum = 0
-        if lum > 255:
-            lum = 255
-        # image.putpixel((px, py), (0, lum, 0))# green map
-        image.putpixel((px, py), (lum, lum, lum))  # greyscale
-        px += 1
-        if px >= w:
-            if (py % 50) == 0:
-                print(f"Line saved {py} of {h}")
-            px = 0
-            py += 1
-            if py >= h:
-                break
-    image = image.resize((w, 4 * h))
-    output_path = "images/" + filename.split("\\")[-1].split(".")[0] + ".png"
-    image.save(output_path)
-
-
 if __name__ == "__main__":
     # filename = 'recordings/noaa18-march-9.wav'
     # original(filename)
